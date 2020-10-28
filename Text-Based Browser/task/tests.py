4bae4fdf89ebda4b57959ba5624e50c3e4580d6c
Victor Kalinin <victor.kalinin@gmail.com>
2020-10-28 21:55:20 +0300

    Home

--- Text-Based Browser/task/tests.py
+++ Text-Based Browser/task/tests.py
@@ -18,50 +18,26 @@ CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 class TextBasedBrowserTest(StageTest):
 
     def generate(self):
+
+        dir_for_files = 'tb_tabs'
         return [
             TestCase(
                 stdin='bloomberg.com\nbloomberg\nexit',
-                attach=('Bloomberg', 'New York Times', 'bloomberg'),
-                args=['tb_tabs']
+                attach='Bloomberg',
+                args=[dir_for_files]
             ),
             TestCase(
                 stdin='nytimes.com\nnytimes\nexit',
-                attach=('New York Times', 'Bloomberg', 'nytimes'),
-                args=['tb_tabs']
+                attach='The New York Times',
+                args=[dir_for_files]
             ),
             TestCase(
                 stdin='nytimescom\nexit',
-                args=['tb_tabs']
+                args=[dir_for_files]
             ),
             TestCase(
-                stdin='blooomberg.com\nexit',
-                args=['tb_tabs']
-            ),
-            TestCase(
-                stdin='blooomberg.com\nnytimes.com\nexit',
-                attach=(None, 'New York Times', 'Bloomberg', 'nytimes'),
-                args=['tb_tabs']
-            ),
-            TestCase(
-                stdin='nytimescom\nbloomberg.com\nexit',
-                attach=(None, 'Bloomberg', 'New York Times', 'bloomberg'),
-                args=['tb_tabs']
-            ),
-            TestCase(
-                stdin='bloomberg.com\nnytimes.com\nback\nexit',
-                attach={
-                    'This New Liquid Is Magnetic, and Mesmerizing': (1, 'New York Times'),
-                    'The Space Race: From Apollo 11 to Elon Musk': (2, 'Bloomberg')
-                },
-                args=['tb_tabs']
-            ),
-            TestCase(
-                stdin='nytimes.com\nbloomberg.com\nback\nexit',
-                attach={
-                    'This New Liquid Is Magnetic, and Mesmerizing': (2, 'New York Times'),
-                    'The Space Race: From Apollo 11 to Elon Musk': (1, 'Bloomberg')
-                },
-                args=['tb_tabs']
+                stdin='bloombergcom\nexit',
+                args=[dir_for_files]
             ),
         ]
 
@@ -75,16 +51,18 @@ class TextBasedBrowserTest(StageTest):
         :return: True, if right_words is present in saved tab
         """
 
-        for path, dirs, files in os.walk(path_for_tabs):
-            for file in files:
-                with open(os.path.join(path_for_tabs, file), 'r') as tab:
-                    try:
-                        content = tab.read()
-                    except UnicodeDecodeError:
-                        return -1
-                    if right_word in content:
-                        return 1
-            break
+        path, dirs, filenames = next(os.walk(path_for_tabs))
+
+        for file in filenames:
+            print("file: {}".format(file))
+            with open(os.path.join(path_for_tabs, file), 'r', encoding='utf-8') as tab:
+                try:
+                    content = tab.read()
+                except UnicodeDecodeError:
+                    return -1
+                print(content)
+                if 'html' in content and right_word in content:
+                    return 1
 
         return 0
 
@@ -92,35 +70,23 @@ class TextBasedBrowserTest(StageTest):
 
         # Incorrect URL
         if attach is None:
-            if 'error' in reply.lower():
-                return CheckResult.correct()
+            if '<p>' in reply:
+                return CheckResult.wrong('You haven\'t checked whether the URL was correct')
             else:
-                return CheckResult.wrong('There was no "error" word, but should be.')
+                return CheckResult.correct()
 
         # Correct URL
-        if isinstance(attach, tuple):
+        if isinstance(attach, str):
+            right_word = attach
 
-            if len(attach) == 4:
-                _, *attach = attach
-                if 'error' not in reply.lower():
-                    return CheckResult.wrong('There was no "error" word, but should be.')
-
-            right_word, wrong_word, correct_file_name = attach
-
-            path_for_tabs = 'tb_tabs'
+            path_for_tabs = os.path.join(os.curdir, 'tb_tabs')
 
             if not os.path.isdir(path_for_tabs):
-                return CheckResult.wrong(
-                    "Can't find a directory \"" + path_for_tabs + "\" "
-                    "in which you should save your web pages.")
+                return CheckResult.wrong("There are no directory for tabs")
 
             check_files_result = self._check_files(path_for_tabs, right_word)
             if not check_files_result:
-                return CheckResult.wrong(
-                    "Seems like you did\'n save the web page "
-                    "\"" + right_word + "\" into the "
-                    "directory \"" + path_for_tabs + "\". "
-                    "This file with page should be named \"" + correct_file_name + "\"")
+                return CheckResult.wrong('There are no correct saved tabs')
             elif check_files_result == -1:
                 return CheckResult.wrong('An error occurred while reading your saved tab. '
                                          'Perhaps you used the wrong encoding?')
@@ -130,24 +96,10 @@ class TextBasedBrowserTest(StageTest):
             except PermissionError:
                 return CheckResult.wrong("Impossible to remove the directory for tabs. Perhaps you haven't closed some file?")
 
-            if wrong_word in reply:
-                return CheckResult.wrong('It seems like you printed wrong variable')
-
-            if right_word in reply:
+            if '<body' in reply and right_word in reply:
                 return CheckResult.correct()
 
-            return CheckResult.wrong('You printed neither bloomberg_com nor nytimes_com')
-
-        if isinstance(attach, dict):
-            for key, value in attach.items():
-                count, site = value
-                real_count = reply.count(key)
-                if reply.count(key) != count:
-                    return CheckResult.wrong(
-                        f'The site "{site}" should be displayed {count} time(s).\n'
-                        f'Actually displayed: {real_count} time(s).'
-                    )
-            return CheckResult.correct()
+            return CheckResult.wrong('You haven\'t print result of request')
 
 
 TextBasedBrowserTest('browser.browser').run_tests()
